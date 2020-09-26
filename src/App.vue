15e807cab8706ea05d954bbcbe77621adfdf89d1
iwenLjr <1816807688@qq.com>
2020-09-26 17:06:50 +0800

    cors跨域

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,23 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import jsonp from "jsonp"
 
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  data(){
+    return{
+      age:30,
+    }
+  },
+  mounted(){
+    let url = "https://www.easy-mock.com/mock/5f6f0151d94dae3ef99d173b/table/list"
+    jsonp(url, ()=>{
+
+    })
   }
 }
 </script>
