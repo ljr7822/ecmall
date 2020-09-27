9fbc1b0710467034b081c8bc5b248ed1d5457c1e
iwenLjr <1816807688@qq.com>
2020-09-28 00:26:45 +0800

    路由的封装

--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
-import { createApp } from 'vue'
+// import { createApp, h } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
 
-createApp(App).mount('#app')
+VTTCue.config.productionTip = false
+
+new Vue({
+    router,
+    render: h => h(App),
+}).$mount('#app')
+
+// createApp(App).mount('#app')
