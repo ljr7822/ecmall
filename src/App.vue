e861c02152c1d2b0b5aaf073e7f0e48c9b4f43b6
iwenLjr <1816807688@qq.com>
2020-09-27 15:22:10 +0800

    项目目录结构设置

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 import jsonp from "jsonp"
-
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    
   },
   data(){
     return{
-      age:30,
+
     }
   },
   mounted(){
-    let url = "https://www.easy-mock.com/mock/5f6f0151d94dae3ef99d173b/table/list"
-    jsonp(url, ()=>{
-
-    })
+    
   }
 }
 </script>
